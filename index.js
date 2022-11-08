c34733546e5c288947accccb690b7f73c7645a3a
morshed0099 <mdgolammorshed0099@gmail.com>
2022-11-09 00:55:28 +0600

    service limit data send

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const cors=require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-app.use(cors())
-app.use(express.json())
 
+app.use(cors());
+app.use(express.json());
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1m4kiwj.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const servicesCollection=client.db('yourPhotodb').collection('services');
 async function run(){
     try{
-
+      app.get('/service',async(req,res)=>{
+        const query={}
+        const cursor=servicesCollection.find(query)
+        const service= await cursor.limit(3).toArray();
+        console.log(service);
+        res.send(service)
+      })
     }
     finally{
 
